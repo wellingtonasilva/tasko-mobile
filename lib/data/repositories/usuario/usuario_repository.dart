c7e3dbe594574038ce5ce72013625c6b2c214206
Wellington Silva <wellingtonasilva@gmail.com>
2026-04-27 10:15:23 +0100

    Fluxo de Login

---
+++ lib/data/repositories/usuario/usuario_repository.dart
@@ -0,0 +1,18 @@
+import 'package:tasko_mobile/domain/usuario/request/adicionar_usuario_request.dart';
+import 'package:tasko_mobile/domain/usuario/request/atualizar_usuario_request.dart';
+import 'package:tasko_mobile/domain/usuario/request/login_request.dart';
+import 'package:tasko_mobile/domain/usuario/response/usuario_login_response.dart';
+import 'package:tasko_mobile/domain/usuario/response/usuario_response.dart';
+import 'package:tasko_mobile/util/result.dart';
+
+abstract class UsuarioRepository {
+  Future<Result<List<UsuarioResponse>>> listar();
+  Future<Result<UsuarioResponse>> obterPorId(int id);
+  Future<Result<void>> excluir(int id);
+  Future<Result<UsuarioResponse>> adicionar(AdicionarUsuarioRequest request);
+  Future<Result<UsuarioResponse>> atualizar(
+    int id,
+    AtualizarUsuarioRequest request,
+  );
+  Future<Result<UsuarioLoginResponse>> login(LoginRequest request);
+}
