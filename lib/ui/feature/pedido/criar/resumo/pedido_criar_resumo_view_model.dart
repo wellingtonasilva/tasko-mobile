62e5f10ebc915f19fbc8df5db43a361a46326ba1
Wellington Silva <wellingtonasilva@gmail.com>
2026-05-03 21:34:04 +0100

    Correção na atualização do Pedido

--- lib/ui/feature/pedido/criar/resumo/pedido_criar_resumo_view_model.dart
+++ lib/ui/feature/pedido/criar/resumo/pedido_criar_resumo_view_model.dart
@@ -71,7 +71,7 @@ class PedidoCriarResumoViewModel extends Notifier<PedidoCriarResumoUiState> {
         condicaoPagamentoId: rascunho.condicaoPagamentoId,
         latitude: rascunho.latitude,
         longitude: rascunho.longitude,
-        empresaId: 8,
+        empresaId: await ref.read(authLocalStorageProvider).getEmpresaId() ?? 0,
       );
       result = await ref
           .read(pedidoRepositoryHybridProvider)
